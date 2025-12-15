f8dd75b3dd32ed4ffa17ea464769b98a512e7399
Belén Albeza <belen@hey.com>
2025-12-15 12:38:14 +0100

    Set up custom favicon

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -12,20 +12,24 @@ import type { Route } from "./+types/root";
 import { BookHeart } from "lucide-react";
 import "./app.css";
 
-// TODO: Uncomment this to use Google Fonts
-//
-// export const links: Route.LinksFunction = () => [
-//   { rel: "preconnect", href: "https://fonts.googleapis.com" },
-//   {
-//     rel: "preconnect",
-//     href: "https://fonts.gstatic.com",
-//     crossOrigin: "anonymous",
-//   },
-//   {
-//     rel: "stylesheet",
-//     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-//   },
-// ];
+export const links: Route.LinksFunction = () => [
+  {
+    rel: "icon",
+    href: "/favicon.svg",
+    type: "image/svg+xml",
+  },
+  // TODO: Uncomment this to use Google Fonts
+  //   { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  //   {
+  //     rel: "preconnect",
+  //     href: "https://fonts.gstatic.com",
+  //     crossOrigin: "anonymous",
+  //   },
+  //   {
+  //     rel: "stylesheet",
+  //     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+  //   },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
